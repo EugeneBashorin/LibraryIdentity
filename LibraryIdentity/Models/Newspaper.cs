f84efc2613e09a263b4ff65b25b5ed1d350b7d8d
Eugene Bashorin <bashorinjeka@gmail.com>
2017-09-06 08:35:03 +0300

    Added nessesary/ deleted unnessesary classes

---
+++ LibraryIdentity/Models/Newspaper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryIdentity.Models
+{
+    [Serializable]
+    public class Newspaper : PrintEdition
+    {
+        public string Category { get; set; }
+    }
+}
---
+++ LibraryIdentity/Models/PrintEdition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryIdentity.Models
+{
+    [Serializable]
+    public abstract class PrintEdition
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Publisher { get; set; }
+        public int Id { get; set; }
+    }
+}
